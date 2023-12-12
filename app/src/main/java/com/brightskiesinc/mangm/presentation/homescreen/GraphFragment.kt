442e54ede093a2add67542328b1ca3d8904baf4f
Honey Abdelkader <honey.abdelkader.ext@brightskiesinc.com>
2023-12-12 17:16:39 +0200

    implemented most of graph design

--- app/src/main/java/com/brightskiesinc/mangm/presentation/homescreen/GraphFragment.kt
+++ app/src/main/java/com/brightskiesinc/mangm/presentation/homescreen/GraphFragment.kt
@@ -1,6 +1,7 @@
 package com.brightskiesinc.mangm.presentation.homescreen
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +11,11 @@ import com.brightskiesinc.mangm.presentation.base.BaseFragment
 import com.brightskiesinc.mangmapp.R
 import com.brightskiesinc.mangmapp.databinding.FragmentGraphBinding
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-
 
 class GraphFragment : BaseFragment<FragmentGraphBinding>() {
     override fun onCreateView(
@@ -33,68 +31,78 @@ class GraphFragment : BaseFragment<FragmentGraphBinding>() {
         setDataToLineChart()
     }
 
+    //from vm we need max, min, avg, points, (maybe each point comes with green up or red down and its +- val)
 
     private fun setUpLineChart() {
-        with(binding.lineChart) {
+        val lineChart = binding.lineChart
+        val max = 2890f
+        val min = 2870f
+        val avg = 2880f
+        with(lineChart) {
             animateX(1200, Easing.EaseInSine)
+            //simplify graph
             description.isEnabled = false
+            legend.isEnabled = false
             xAxis.isEnabled = false
-
-            val yAxis: YAxis = binding.lineChart.axisLeft
-
-            yAxis.setDrawGridLines(false)
-
-//            xAxis.setDrawGridLines(false)
-//            xAxis.position = XAxis.XAxisPosition.BOTTOM
-//            xAxis.granularity = 1F
-//            xAxis.valueFormatter = MyAxisFormatter()
-
             axisRight.isEnabled = false
-            extraRightOffset = 30f
+            axisLeft.setDrawGridLines(false)
+            axisLeft.setDrawAxisLine(false)
+            axisLeft.labelCount = 3
+            axisLeft.axisMaximum = max
+            axisLeft.axisMinimum = min
+            axisLeft.spaceTop = 10f
+            axisLeft.spaceBottom = 10f
+
+            //dashed zero line
+            val limitLine = LimitLine(avg)
+            limitLine.lineWidth = 1f
+            limitLine.lineColor = Color.LTGRAY
+            limitLine.enableDashedLine(10f, 10f, 0f)
+            axisLeft.addLimitLine(limitLine)
+
+            //disable zoom
+            lineChart.isDragEnabled = false
+            lineChart.isDoubleTapToZoomEnabled = false
+            lineChart.isScaleXEnabled = false
+            lineChart.isScaleYEnabled = false
+
+
         }
     }
 
     private fun week1(): ArrayList<Entry> {
         val sales = ArrayList<Entry>()
-        sales.add(Entry(0f, 15f))
-        sales.add(Entry(1f, 16f))
-        sales.add(Entry(2f, 13f))
-        sales.add(Entry(3f, 22f))
-        sales.add(Entry(4f, 20f))
+        sales.add(Entry(0f, 2875f))
+        sales.add(Entry(1f, 2870f))
+        sales.add(Entry(2f, 2890f))
+        sales.add(Entry(3f, 2885f))
+        sales.add(Entry(4f, 2880f))
         return sales
-    }
+    } //only for ui testing
 
     @SuppressLint("ResourceType")
     private fun setDataToLineChart() {
+        val lineChart = binding.lineChart
 
-        val weekOneSales = LineDataSet(week1(), "Week 1")
-        weekOneSales.lineWidth = 3f
-//        weekOneSales.valueTextSize = 15f
-        weekOneSales.mode = LineDataSet.Mode.CUBIC_BEZIER
-        weekOneSales.color = ContextCompat.getColor(requireContext(), R.color.mangm_orange)
+        val valuesPlotLine = LineDataSet(week1(), "Week 1")
+        valuesPlotLine.lineWidth = 3f
+        valuesPlotLine.mode = LineDataSet.Mode.CUBIC_BEZIER
+        valuesPlotLine.setDrawValues(false)
+        valuesPlotLine.setDrawCircles(false)
+        valuesPlotLine.color = ContextCompat.getColor(requireContext(), R.color.mangm_orange)
+        valuesPlotLine.highLightColor = Color.GRAY
+        valuesPlotLine.setDrawHorizontalHighlightIndicator(false)
+        valuesPlotLine.setDrawFilled(true)
+        valuesPlotLine.fillColor = ContextCompat.getColor(requireContext(), R.color.mangm_orange)
+        valuesPlotLine.fillAlpha = 15
 
 
         val dataSet = ArrayList<ILineDataSet>()
-        dataSet.add(weekOneSales)
-
+        dataSet.add(valuesPlotLine)
         val lineData = LineData(dataSet)
-        binding.lineChart.data = lineData
-
-        binding.lineChart.invalidate()
+        lineChart.data = lineData
+//        lineChart.notifyDataSetChanged() //to be implemented
+        lineChart.invalidate()
     }
 
-
-    inner class MyAxisFormatter : IndexAxisValueFormatter() {
-
-        private var items = arrayListOf("1", "2", "3", "4", "5")
-
-        override fun getAxisLabel(value: Float, axis: AxisBase?): String? {
-            val index = value.toInt()
-            return if (index < items.size) {
-                items[index]
-            } else {
-                null
-            }
-        }
-    }
 }
